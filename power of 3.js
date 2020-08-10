78fb93986919ea42352a50fb0ddbc105d97e283b
Anton <52634903+skyman149@users.noreply.github.com>
2020-08-09 20:31:15 -0700

    Create power of 3.js

---
+++ power of 3.js
@@ -0,0 +1,16 @@
+/*
+Given a positive integer N, return the largest integer k such that 3^k < N.
+
+For example,
+
+largest_power(3) == 0
+largest_power(4) == 1
+You may assume that the input to your function is always a positive integer.
+*/
+function largestPower(n){
+    let result = 0
+    while(3**result < n) {
+      result++;
+      }
+    return result-1;
+  }
