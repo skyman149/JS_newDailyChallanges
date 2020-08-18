6a01071e22ad8c4b671076a2952b6fe73c621a10
Anton <52634903+skyman149@users.noreply.github.com>
2020-08-17 19:25:37 -0700

    Update Number.isSafeInteger() Is integer safe to use.js

---
+++ Number.isSafeInteger() Is integer safe to use.js
@@ -0,0 +1,11 @@
+/*
+Not all integers can be represented by JavaScript/TypeScript. 
+It has space to to represent 53bit signed integers. In this Kata, we've 
+to determine if it is safe to use the integer or not. Make use of the latest 
+ES6 features to find this.
+*/
+
+function SafeInteger(n) {
+    return Number.isSafeInteger(n);
+  }
+
