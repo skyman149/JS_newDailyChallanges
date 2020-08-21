d9d739414e8ff2a97a253c2638d11e4809029a48
Anton <52634903+skyman149@users.noreply.github.com>
2020-08-20 21:20:01 -0700

    Create Sum of Minimums!.js

---
+++ Sum of Minimums!.js
@@ -0,0 +1,15 @@
+/*
+Given a 2D array of size m * n. Your task is to find the sum of minimum 
+value in each row.
+For Example:
+[
+  [1, 2, 3, 4, 5],       // minimum value of row is 1
+  [5, 6, 7, 8, 9],       // minimum value of row is 5
+  [20, 21, 34, 56, 100]  // minimum value of row is 20
+]
+So, the function should return 26 because sum of minimums is as 1 + 5 + 20 = 26
+*/
+
+function sumOfMinimums(arr) {
+    return arr.reduce( (acc, cur) => acc + Math.min(...cur), 0);
+   }
