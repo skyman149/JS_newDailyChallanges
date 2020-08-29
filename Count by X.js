9261f5ee008c2dc0151fb66d9c1ca4bd3a1bda2a
Anton <52634903+skyman149@users.noreply.github.com>
2020-08-29 14:28:32 -0700

    Create Count by X.js

---
+++ Count by X.js
@@ -0,0 +1,17 @@
+/*
+Create a function with two arguments that will return an array of the first (n) 
+multiples of (x).
+
+Assume both the given number and the number of times to count will be positive 
+numbers greater than 0.
+
+Return the results as an array
+*/
+
+function countBy(x, n) {
+    var z = [];
+    for (let i = x; i<=x*n; i+=x){
+      z.push(i);
+    }
+    return z;
+  }
