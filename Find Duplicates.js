41fe2fb3c91de618fcebc528f839cca9d5233e8b
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-02 11:52:53 -0700

    Create Find Duplicates.js

---
+++ Find Duplicates.js
@@ -0,0 +1,17 @@
+/*
+Given an array, find the duplicates in that array, and return a new array of those 
+duplicates. The elements of the returned array should appear in the order when they 
+first appeared as duplicates.
+
+Note: numbers and their corresponding string representations should not be treated
+ as duplicates (i.e., "1" != 1).
+
+Examples
+[1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]  ==>  [4, 3, 1]
+[0, 1, 2, 3, 4, 5]                ==>  []
+*/
+
+function duplicates(arr) {
+    let nArr = arr.filter((el,i) => i === arr.indexOf(el) && i !== arr.lastIndexOf(el))
+    return nArr;
+  }
