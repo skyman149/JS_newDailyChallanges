3aa8c0518a0aaa7ddfc01f54d53b8f2ac119d73f
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-03 12:58:06 -0700

    Create .join() -- Sentence Smash.js

---
+++ .join() -- Sentence Smash.js
@@ -0,0 +1,9 @@
+/*
+Write a function that takes an array of words and smashes them together into 
+a sentence and returns the sentence. You can ignore any need to sanitize words
+ or add punctuation, but you should add spaces between each word. 
+*/
+
+function smash (words) {
+    return words.join(' ');
+  }
