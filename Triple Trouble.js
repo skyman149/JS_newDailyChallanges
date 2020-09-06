b8f6b93c98e8097fdccd418dfbae81114587136b
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-06 13:39:09 -0700

    Create Triple Trouble.js

---
+++ Triple Trouble.js
@@ -0,0 +1,16 @@
+/*
+Create a function that will return a string that combines all of the letters
+ of the three inputed strings in groups. Taking the first letter of all of the
+  inputs and grouping them next to each other. Do this for every letter, see 
+  example below!
+
+E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+*/
+
+function tripleTrouble(one, two, three){
+    let str = ''
+    for (let i = 0; i < one.length; i++) {
+      str += `${one[i]}${two[i]}${three[i]}`
+    }
+    return str;
+  }
