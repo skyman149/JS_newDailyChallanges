3a590e8d7ea369eb8c7c1d12179e5ed3501c55a0
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-07 17:16:05 -0700

    Create str.toLowerCase() -- case -- Bartender, drinks!.js

---
+++ str.toLowerCase() -- case -- Bartender, drinks!.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/568dc014440f03b13900001d/train/javascript
+*/
+
+function getDrinkByProfession(param){
+    let lowParam = param.toLowerCase()
+    switch(lowParam) {
+    case 'jabroni': return 'Patron Tequila'; break;
+    case 'school counselor': return 'Anything with Alcohol'; break;
+    case 'programmer': return 'Hipster Craft Beer'; break;
+    case 'bike gang member': return 'Moonshine'; break;
+    case 'politician': return 'Your tax dollars'; break;
+    case 'rapper': return 'Cristal'; break;
+    default: return 'Beer' 
+  }
+  }
