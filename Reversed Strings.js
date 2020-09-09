5497776aee33738e9f7ff3e9a8669efa0a6ee383
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-08 20:27:29 -0700

    Update Reversed Strings.js

--- Reversed Strings.js
+++ Reversed Strings.js
@@ -7,3 +7,10 @@ function solution(str){
     }
     return str1;
   }
+
+// OR
+
+function solution(str){
+    return str.split("").reverse().join("");
+  }
+  //spilt -> [w, o, r, l, d], reverse -> [d, l, r, o, w], join -> 'dlrow'
