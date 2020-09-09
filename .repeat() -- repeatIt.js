0ed8741b79bb7e2365a5cff2a0c02c5e4948a572
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-08 20:20:58 -0700

    Create .repeat() -- repeatIt.js

---
+++ .repeat() -- repeatIt.js
@@ -0,0 +1,10 @@
+var repeatIt = function(str, n) {
+    return typeof str === 'string'? str.repeat(n) : "Not a string";
+  }
+
+/*
+The function should return a string that repeats the input string n number of times.
+
+If anything other than a string is passed in you should return "Not a string"
+
+*/
