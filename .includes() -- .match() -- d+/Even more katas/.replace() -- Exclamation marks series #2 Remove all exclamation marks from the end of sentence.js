586cccf58caad533588d2ca144b032cf6672ca74
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-12 15:41:11 -0700

    Create .replace() -- Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js

---
+++ .includes() -- .match() -- d+/Even more katas/.replace() -- Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js
@@ -0,0 +1,15 @@
+/*
+Remove all exclamation marks from the end of sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
+
+function remove(s){
+    return s.replace(/!+$/, '');
+  }
