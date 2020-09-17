f924405994e2b65e531c457f43a266d221c16c8c
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-16 17:06:01 -0700

    Create {} Object in array -- Add property to every object in array.js

---
+++ .includes() -- .match() -- d+/Even more katas/{} Object in array -- Add property to every object in array.js
@@ -0,0 +1,6 @@
+/*
+Your task is to add a new property usersAnswer to every object in the array questions. 
+The value of usersAnswer should be set to null. The solution should work for array of 
+any length.
+*/
+questions.forEach(el => el.usersAnswer = null);
