93e4666fe90c3a877a53d2428e3ec928374293aa
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-18 19:49:54 -0700

    Update Squares sequence.js

--- Squares sequence.js
+++ Squares sequence.js
@@ -7,5 +7,20 @@ Examples
 2, 5  -->  [2, 4, 16, 256, 65536]
 3, 3  -->  [3, 9, 81]
 */
+
 //Not my solution:
+
 const squares = (x, n) => Array.from({ length: n }, (_, index) => x ** (2 ** index))
+
+// BEST SOLUTION FOR BEGINERS:
+
+function squares(x, n) {
+    let arr = [];
+    let i = 1;
+    while (i <= n){
+      arr.push(x);
+      x = x ** 2;
+      i++;
+    }
+    return arr;
+  }
