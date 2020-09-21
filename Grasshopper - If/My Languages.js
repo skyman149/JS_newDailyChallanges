3c00a6429298a8270591fb86603864bd38fe80ce
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-20 20:48:40 -0700

    Create My Languages.js

---
+++ Grasshopper - If/My Languages.js
@@ -0,0 +1,26 @@
+/*
+You are given a dictionary/hash/object containing some languages and your test results 
+in the given languages. Return the list of languages where your test score is at least 
+60, in descending order of the results.
+
+Note: the scores will always be unique (so no duplicate values)
+
+Examples
+{"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+{"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+{"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+*/
+
+function myLanguages(results) {
+    let valArr = Object.entries(results);
+    let sorted = valArr.sort((a,b) => b[1]-a[1])
+    let resArr = [];
+    for (let i = 0; i<sorted.length; i++){
+      if(sorted[i][1] >= 60){resArr.push(sorted[i][0])}
+    }
+    return resArr;
+  }
+
+  //OR
+
+  
