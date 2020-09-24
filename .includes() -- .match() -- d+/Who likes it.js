a4cd2ccbe536167522a249427cbb1d35a7f5c046
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-24 15:35:02 -0700

    Create Who likes it.js

---
+++ .includes() -- .match() -- d+/Who likes it.js
@@ -0,0 +1,17 @@
+/*
+likes [] -- must be "no one likes this"
+likes ["Peter"] -- must be "Peter likes this"
+likes ["Jacob", "Alex"] -- must be "Jacob and Alex like this"
+likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
+likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
+*/
+
+//my code:
+
+function likes(names) {
+    if (names.length === 0){return "no one likes this";}
+    if (names.length === 1){return `${names[0]} likes this`;}
+    if (names.length === 2){return `${names[0]} and ${names[1]} like this`;}
+    if (names.length === 3){return `${names[0]}, ${names[1]} and ${names[2]} like this`;}
+    if (names.length >=  4){return `${names[0]}, ${names[1]} and ${names.length-2} others like this`;}
+  }
