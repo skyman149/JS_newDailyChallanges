931d73117073d4c9ed6d5d29c02351a90d4f0dda
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-23 19:01:09 -0700

    Update Simple consecutive pairs.js

--- .includes() -- .match() -- d+/Simple consecutive pairs.js
+++ .includes() -- .match() -- d+/Simple consecutive pairs.js
@@ -20,3 +20,12 @@ function pairs(ar){
      }
     return arr.length;
    }
+
+   //OR
+
+   function pairs(arr){
+    let count = 0;
+    for (let i = 0; i < arr.length; i += 2)
+      if (arr[i]-1 === arr[i+1] || arr[i]+1 === arr[i+1]){count++;}
+    return count;
+  };
