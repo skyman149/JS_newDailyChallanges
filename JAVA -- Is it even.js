4eaed9c43f24b4e45906654fefc2d827729adc18
Anton <52634903+skyman149@users.noreply.github.com>
2020-09-28 18:14:36 -0700

    Create JAVA -- Is it even.js

---
+++ JAVA -- Is it even.js
@@ -0,0 +1,11 @@
+public class Number {
+
+    public boolean isEven(double n) {
+        if((n % 2) == 0)
+          return true;
+        else 
+          return false;
+    }
+  }
+
+  // even or odd?
