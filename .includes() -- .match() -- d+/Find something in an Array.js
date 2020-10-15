3cfe40d2033af768bbe99c156e34594713a5430a
Anton <52634903+skyman149@users.noreply.github.com>
2020-10-15 14:38:54 -0700

    Create Find something in an Array.js

---
+++ .includes() -- .match() -- d+/Find something in an Array.js
@@ -0,0 +1,14 @@
+/*
+Create a find function that takes a string and an array as arguments.
+ If the string is in the array, return true.
+
+For example:
+
+find("hello", ["bye bye","hello"]) // return true
+find("anything", ["bye bye","hello"]) // return false
+Note: "Hello" !== "hello" // case-sensitive comparison
+*/
+
+var find = function(string, array) {
+    return string === "" ? false : array.join("").includes(string);
+  };
