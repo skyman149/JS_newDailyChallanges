41a3aaf2bc5a55c20b941cc4714020cfc75c0213
Anton <52634903+skyman149@users.noreply.github.com>
2020-10-19 15:00:40 -0700

    Update Training JS 34 methods of Math---pow() sqrt() and cbrt().js

--- Training JS 34 methods of Math---pow() sqrt() and cbrt().js
+++ Training JS 34 methods of Math---pow() sqrt() and cbrt().js
@@ -2,3 +2,9 @@ function cutCube(volume,n){
     const isOK = x => (Math.round(Math.cbrt(x)) ** 3 === x);
      return isOK(volume / n) && isOK(n);
  }
+
+ //OR
+
+ function cutCube(volume,n){
+    return Math.round(Math.cbrt(volume / n)) ** 3 === volume/n && Math.round(Math.cbrt(n)) ** 3 === n;
+  }
