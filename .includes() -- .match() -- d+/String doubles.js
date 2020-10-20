a2b3793f72061f16e53e2162b4bf3c7a0f642956
Anton <52634903+skyman149@users.noreply.github.com>
2020-10-20 15:02:01 -0700

    Create String doubles.js

---
+++ .includes() -- .match() -- d+/String doubles.js
@@ -0,0 +1,11 @@
+function doubles(s){
+    let arr = [];
+    for (let i = 0; i < s.length; i++){
+      if (arr[arr.length-1] === s[i]){
+        arr.pop();
+      } else {
+        arr.push(s[i]);
+      }
+    }
+    return arr.join("")
+  }
