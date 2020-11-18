6ddac37b2794dd232cca29aa059e263bc9789d06
Anton <52634903+skyman149@users.noreply.github.com>
2020-11-17 17:14:15 -0800

    Update Multiplication table.js

--- Multiplication table.js
+++ Multiplication table.js
@@ -10,4 +10,22 @@ multiplicationTable = function(size) {
      }
     return arr;
     }
+
+// OR
+
+multiplicationTable = function(size) {
+  
+  let arr = [];
+  
+  for(let i = 1; i <= size; i++){
+    let inArr = [];
+    for(let j = 1; j <= size; j++){
+      inArr.push(j * i);
+    }
+    arr.push(inArr);
+  }
+  
+  return arr;
+}
+
   
