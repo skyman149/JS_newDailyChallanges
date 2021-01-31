ce10e1e0b53ac74ffb60967d204809ccbf4f08a9
Anton <52634903+skyman149@users.noreply.github.com>
2021-01-30 19:29:22 -0800

    Create Leet -- SumOfTwo.js

---
+++ .includes() -- .match() -- d+/Leet -- SumOfTwo.js
@@ -0,0 +1,14 @@
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++){
+        if(nums[i] + nums[i+1] === 9) return [i, i+1];
+    }
+};
