44e0bc89041bc1f05cdc8fc605d4c066c9f1b33f
Anton <52634903+skyman149@users.noreply.github.com>
2021-01-31 08:55:16 -0800

    Create Leet -- 13. Roman to Integer.js

---
+++ folder1/Leet -- 13. Roman to Integer.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    
+    let rom = {
+        I:1,
+        V:5,
+        X:10,
+        L:50,
+        C:100,
+        D:500,
+        M:1000
+    };
+      let res = 0;
+      for(let i = 0; i < s.length; i++){
+      if (rom[s[i]] < rom[s[i+1]]){
+        res -= rom[s[i]];
+      } else {
+          res += rom[s[i]];
+      }
+    }
+      return res;
+  };
