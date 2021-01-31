fafe7df181ef9b37cdf27fa059f2d5a5345440fa
Anton <52634903+skyman149@users.noreply.github.com>
2021-01-30 21:04:27 -0800

    Create Multiply the number.js

---
+++ .includes() -- .match() -- d+/Multiply the number.js
@@ -0,0 +1,10 @@
+function multiply(number){
+    let abs = Math.abs(number)
+    let numLen = (Math.abs(number)+"").split("").length;
+    let result = abs;
+    while (numLen > 0){
+      result *= 5;
+      numLen--;
+    }
+    return number < 0 ? -result : result;
+  }
