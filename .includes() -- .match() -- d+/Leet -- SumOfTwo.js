f87b362767d95a05d675995a65a7173cecf62c13
Anton <52634903+skyman149@users.noreply.github.com>
2021-01-30 19:42:45 -0800

    Update Leet -- SumOfTwo.js

--- .includes() -- .match() -- d+/Leet -- SumOfTwo.js
+++ .includes() -- .match() -- d+/Leet -- SumOfTwo.js
@@ -8,7 +8,10 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++){
-        if(nums[i] + nums[i+1] === 9) return [i, i+1];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] === target - nums[i]) return [i, j]
+        }
     }
+    return "No two sum solution";
 };
