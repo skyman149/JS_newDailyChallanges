c4b1fa546780032f5c0e2a7d8dc525e702a6afcc
Anton <52634903+skyman149@users.noreply.github.com>
2021-02-02 07:15:02 -0800

    Create extract domain name.js

---
+++ folder1/extract domain name.js
@@ -0,0 +1,5 @@
+function domainName(url){
+    let str = url.replace('http://','').replace('https://','').replace('www.','').split(/[//]/)[0];
+        let res = str.split(".")[0];
+        return res;
+  }
