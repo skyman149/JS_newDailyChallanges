8385b0ff0691745326ac65e5339ebc6a903c058f
Anton <52634903+skyman149@users.noreply.github.com>
2021-02-14 19:37:54 -0800

    Create Anagram Detection.js

---
+++ .includes() -- .match() -- d+/Even more katas/Anagram Detection.js
@@ -0,0 +1,5 @@
+function isAnagram (test, original) {
+    return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("");
+  }
+
+  // https://www.codewars.com/kata/529eef7a9194e0cbc1000255/train/javascript
