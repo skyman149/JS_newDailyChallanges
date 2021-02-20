c8d5259d037825c50391db342d771bee6c564583
Anton <52634903+skyman149@users.noreply.github.com>
2021-02-20 11:01:48 -0800

    Update Blood-Alcohol Content.js

--- .includes() -- .match() -- d+/Even more katas/Blood-Alcohol Content.js
+++ .includes() -- .match() -- d+/Even more katas/Blood-Alcohol Content.js
@@ -28,3 +28,4 @@ function bloodAlcoholContent(drinks, weight, sex, time){
     let bac = drinks.ounces * drinks.abv * 5.14 / weight * (sex === 'male' ? 0.73 : 0.66) - 0.015 * time;
     return +bac.toFixed(4);
    }
+   // -
