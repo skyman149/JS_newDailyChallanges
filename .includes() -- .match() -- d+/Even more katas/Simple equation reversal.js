cf157c2b9c252e4e2efdbb8a401aa9e20e0e23d3
Anton <52634903+skyman149@users.noreply.github.com>
2021-03-04 17:50:03 -0800

    Create Simple equation reversal.js

---
+++ .includes() -- .match() -- d+/Even more katas/Simple equation reversal.js
@@ -0,0 +1,11 @@
+function solve(eq){
+    let nums = eq.split(/[*/+-]/).reverse();
+    let symbols = eq.split(/[a-z,0-9]/).reverse().filter(el => el != "");
+    let maxLength = Math.max(nums.length, symbols.length);
+    let result = [];
+    for (let i = 0; i < maxLength; i++) {
+      result.push(nums[i]);
+      result.push(symbols[i]);
+    }
+    return result.join("");
+  }
