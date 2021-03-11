2148d330fdeaa6e2df22e7ad1a9b09766c3834b2
Anton <52634903+skyman149@users.noreply.github.com>
2021-03-10 18:12:49 -0800

    Create Return String of First Characters.js

---
+++ .includes() -- .match() -- d+/Even more katas/Return String of First Characters.js
@@ -0,0 +1,14 @@
+function makeString(s){
+  
+    let arr = s.split(" ");
+    let result = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+      result.push(arr[i][0]);
+    }
+    
+    return result.join("");
+  }
+
+  
+  
