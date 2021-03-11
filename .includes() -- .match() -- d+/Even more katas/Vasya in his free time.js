3eee60b28aa04ae8a83485e197b1ba4d3422315d
Anton <52634903+skyman149@users.noreply.github.com>
2021-03-10 19:34:41 -0800

    Update Vasya in his free time.js

--- .includes() -- .match() -- d+/Even more katas/Vasya in his free time.js
+++ .includes() -- .match() -- d+/Even more katas/Vasya in his free time.js
@@ -1,4 +1,16 @@
 function CalculateString( n,  nums){
+  
+    while(nums.includes('10') || nums.includes('01')){
+      nums = nums.replace(/10/g, '');
+      nums = nums.replace(/01/g, '');
+    }
+    
+    return nums.length;;
+  }
+
+  // OR without n
+
+  function CalculateString( n,  nums){
     let countO = 0;
     let countI = 0;
     for (let i = 0; i < nums.length; i++) {
