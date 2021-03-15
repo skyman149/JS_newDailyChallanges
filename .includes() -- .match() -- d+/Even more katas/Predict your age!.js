0bca28d9bcb89f7a675feccf8ac41542e3cf412d
Anton <52634903+skyman149@users.noreply.github.com>
2021-03-14 17:27:46 -0700

    Update Predict your age!.js

--- .includes() -- .match() -- d+/Even more katas/Predict your age!.js
+++ .includes() -- .match() -- d+/Even more katas/Predict your age!.js
@@ -10,3 +10,10 @@ function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
     
     return Math.floor(Math.sqrt(result) / 2);
   }
+
+  // OR
+
+  function predictAge(...age){
+    let arr = [...age]
+     return Math.floor(Math.sqrt(arr.map(el => el * el).reduce((a, b) => a + b)) / 2);
+   }
