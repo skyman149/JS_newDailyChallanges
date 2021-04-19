530529ec019b5910f47ad663b9c8145cff5d6ca8
Anton <52634903+skyman149@users.noreply.github.com>
2021-04-18 21:03:50 -0700

    Create Printer Errors.js

---
+++ .includes() -- .match() -- d+/Even more katas/Printer Errors.js
@@ -0,0 +1,28 @@
+// hard code solution
+
+function printerError(s) {
+    
+    let count1 = 0;
+    let count2 = s.length;
+  
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === "n" ||
+         s[i] === "o" ||
+         s[i] === "p" ||
+         s[i] === "q" ||
+         s[i] === "r" ||
+         s[i] === "s" ||
+         s[i] === "t" ||
+         s[i] === "u" ||
+         s[i] === "v" ||
+         s[i] === "w" ||
+         s[i] === "x" ||
+         s[i] === "y" ||
+         s[i] === "z") count1++;
+    }
+     
+      let res = `${count1}/${count2}`;
+  
+      return res;
+      
+}
