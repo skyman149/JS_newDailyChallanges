95e88f56b8a63c1ce14296e865c764520d11e996
Anton <52634903+skyman149@users.noreply.github.com>
2021-04-22 21:23:09 -0700

    Update Good vs Evil.js

--- .includes() -- .match() -- d+/Even more katas/Good vs Evil.js
+++ .includes() -- .match() -- d+/Even more katas/Good vs Evil.js
@@ -22,3 +22,16 @@ function goodVsEvil(good, evil){
     
     return sumGood > sumEvil ? "Battle Result: Good triumphs over Evil" : sumGood === sumEvil ? "Battle Result: No victor on this battle field" : "Battle Result: Evil eradicates all trace of Good";
   }
+
+  // OR
+
+  function goodVsEvil(good, evil){
+
+    const gTeam = [1, 2, 3, 3, 4, 10];
+    const eTeam = [1, 2, 2, 2, 3, 5, 10];
+    
+    const gSum = good.split(' ').reduce((a, b, i) => a + +b * gTeam[i], 0);
+    const eSum = evil.split(' ').reduce((a, b, i) => a + +b * eTeam[i], 0);
+  
+  return gSum > eSum ? "Battle Result: Good triumphs over Evil" : gSum === eSum ? "Battle Result: No victor on this battle field" : "Battle Result: Evil eradicates all trace of Good";
+  }
