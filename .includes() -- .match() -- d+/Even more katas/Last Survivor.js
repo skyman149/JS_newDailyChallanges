db80cc55f005bf7029c0b841d4e18a0e683f2f35
Anton <52634903+skyman149@users.noreply.github.com>
2021-07-11 20:28:17 -0700

    Update Last Survivor.js

--- .includes() -- .match() -- d+/Even more katas/Last Survivor.js
+++ .includes() -- .match() -- d+/Even more katas/Last Survivor.js
@@ -9,3 +9,11 @@ function lastSurvivor(letters, coords) {
     
     return lettersArr.join('');
   }
+
+  // OR
+
+  function lastSurvivor(string, indices) {
+    const arr = [...string];
+    for (const i of indices) arr.splice(i, 1)
+    return arr[0];
+  }
