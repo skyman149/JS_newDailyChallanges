47a0cde903b35629dd78b8f50bb02efa6d6e2e1f
Anton <52634903+skyman149@users.noreply.github.com>
2021-07-26 20:08:37 -0700

    Update this. -- CamelCase Method.js

--- this. -- CamelCase Method.js
+++ this. -- CamelCase Method.js
@@ -3,3 +3,15 @@ String.prototype.camelCase = function() {
     return this.replace(/(?:^|\s)([a-z])/g, (_, x) => x.toUpperCase()).trim()
     
   }
+
+
+  // OR
+
+
+  String.prototype.camelCase=function(){
+    return this.split(' ').map(w => w.slice(0, 1).toUpperCase() + w.slice(1)).join('');
+  }
+
+  // OR
+
+  
